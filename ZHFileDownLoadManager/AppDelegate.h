6010b24745ad74ceb0a92243793405001642b890
walen <walen@walendeimac.local>
2016-05-31 10:20:30 +0800

    Initial Commit

---
+++ ZHFileDownLoadManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZHFileDownLoadManager
+//
+//  Created by walen on 16/5/31.
+//  Copyright © 2016年 walen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZHFileDownLoadManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZHFileDownLoadManager
+//
+//  Created by walen on 16/5/31.
+//  Copyright © 2016年 walen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
